51d8ce13fcbd6e2300c088806e994e184f9fa8af
ZrkllT <zero.kullt@gmail.com>
2023-05-15 22:59:28 -0400

    15-may

---
+++ Mod04/S05/practica_electrodomesticos/assets/js/scripts.js
@@ -0,0 +1,94 @@
+import Electrodomestico from './clases/electrodomestico.js'
+
+var colores = ['Blanco','Gris','Azul','Negro']
+var listElectro = []
+function listaColores(){
+    $('#selColor').html(`
+        <option value="sel">Seleccionar</option>
+        `)
+    /**/
+    for(const color of colores){
+        $('#selColor').append(`
+            <option value="${color}">${color}</color>
+            `)
+    }/**/
+}
+
+function eliminarFila(iad){
+    listElectro.splice(iad,1)
+    dibujarElectro()
+}
+
+function agregarElectro(){
+    let nombre = $('#inNombre').val()
+    let color = $('#selColor').val()
+
+    if(nombre == ''){
+        $('#msg-nombre').removeClass('d-none')
+        $('#msg-nombre').html('Por favor Ingrese el nombre del Articulo')
+        $('#inNombre').css({ "border" : "1px solid red"})
+        return
+    }else{
+        $('#msg-nombre').addClass('d-none')
+    }
+
+    if(color == '' || color == 'sel'){
+        $('#msg-select').removeClass('d-none')
+        $('#msg-select').html('Por favor Seleccione un color para el Articulo')
+        $('#selColor').css({ "border" : "1px solid red"})
+        return
+    }else{
+        $('#msg-select').addClass('d-none')
+    }
+
+    listElectro.push(new Electrodomestico(nombre,color))
+    $('#btnLimpiar').click()
+    dibujarElectro()
+    $('#inNombre').focus()
+}
+
+function dibujarElectro(){
+    $('#tableElectro tbody').html('')
+    listElectro.forEach((electro,index) =>{
+        $('#tableElectro tbody').append(`
+            <tr id="${index}">
+              <td>${index+1}</td>
+              <td>${electro.nombre}</td>
+              <td>${electro.color}</td>
+              <td><button data-key='${index}' class="btn btn-sm btn-danger">X</button></td>
+            </tr>
+            `)
+    })
+}
+
+
+$(document).ready(function(){
+    listaColores()
+    $(document).on('keypress','#inNombre',function(k){
+        $('#msg-nombre').addClass('d-none')
+        $('#inNombre').css({"border" : "1px solid #c8c8c8"})
+        if(k.keyCode === 13){ /*enter*/
+        }
+    })
+
+    $(document).on('change','#selColor',function(){
+        $('#msg-select').addClass('d-none')
+        $('#selColor').css({"border" : "1px solid #c8c8c8"})
+    })
+
+    $(document).on('click','#btnRegistrar',function(event){
+        event.preventDefault()
+        agregarElectro()
+    })
+
+    $(document).on('mouseenter','#tableElectro tbody tr',function(){
+        $(this).css({ "background" : "#FEF9E7", "color": "blue"})
+    })
+    $(document).on('mouseleave','#tableElectro tbody tr',function(){
+        $(this).css({ "background" : "white", "color": "black"})
+    })
+
+    $(document).on('click','#tableElectro tbody button',function(){
+        eliminarFila($(this).data('key'))
+    })
+})
